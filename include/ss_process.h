49f8793d6649fc5a120cb97c6fadb7766645694b
zhengxiaoyu <zhengxiaoyu@eb6960.(none)>
2019-11-08 18:43:50 +0800

    修改了process的结构定义

--- include/ss_process.h
+++ include/ss_process.h
@@ -5,12 +5,12 @@
 
 typedef struct
 {
-    streamMap video_main;
-    streamMap video_slide;
-    streamMap audio_individual;
-    fileMap output_main;
-    fileMap output_slide;
-    fileMap output_overlay;
+    streamMap *video_main;
+    streamMap *video_slide;
+    streamMap *audio_individual;
+    fileMap *output_main;
+    fileMap *output_slide;
+    fileMap *output_overlay;
 }   meetPro;
 
 #endif

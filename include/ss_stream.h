93c384ee9a07a0495cf516b4ed3e93d1f7c9fd73
zhengxiaoyu <zhengxiaoyu@eb6960.(none)>
2019-11-08 18:29:31 +0800

    添加了streamMap结构体定义

---
+++ include/ss_stream.h
@@ -0,0 +1,19 @@
+#ifndef _SS_STREAM_H_
+#define _SS_STREAM_H_
+
+#include "ss_file.h"
+#include "ss_codec.h"
+#include "ss_filter.h"
+
+typedef struct
+{
+    fileMap *input_fm;
+    codecMap *codecmap;
+    filterMap *filtermap;
+    int cur_index_pkt_in;
+    int cur_index_pkt_out;
+    int cur_index_frame_in;
+    int cur_index_frame_out;
+}   streamMap;
+
+#endif

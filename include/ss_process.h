23eb48c6c2e739b4285c0e2db6de5ac478319835
zhengxiaoyu <zhengxiaoyu@eb6960.(none)>
2019-11-08 18:41:34 +0800

    添加了meetPro结构体的定义

---
+++ include/ss_process.h
@@ -0,0 +1,16 @@
+#ifndef _SS_PROCESS_H_
+#define _SS_PROCESS_H_
+
+#include "ss_stream.h"
+
+typedef struct
+{
+    streamMap video_main;
+    streamMap video_slide;
+    streamMap audio_individual;
+    fileMap output_main;
+    fileMap output_slide;
+    fileMap output_overlay;
+}   meetPro;
+
+#endif

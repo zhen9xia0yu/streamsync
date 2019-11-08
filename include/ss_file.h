6cf510b4ad668ccb73d6ea4aa9f5f93342d77b96
zhengxiaoyu <zhengxiaoyu@eb6960.(none)>
2019-11-08 17:46:43 +0800

    添加了fileMap结构体定义

---
+++ include/ss_file.h
@@ -0,0 +1,16 @@
+#ifndef _SS_FILE_H_
+#define _SS_FILE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libavformat/avformat.h>
+
+typedef struct{
+    const char *filename;
+    const char *ops;
+    AVFormatContext *fmt_ctx;
+    AVOutputFormat *ofmt;`
+}   fileMap;
+
+#endif

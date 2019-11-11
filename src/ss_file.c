f065d7c97fa8ed39bd30b89f5f21913defd1be57
zhengxiaoyu <zhengxiaoyu@eb6960.(none)>
2019-11-11 19:40:19 +0800

    添加了set_input()打开输入流函数

--- src/ss_file.c
+++ src/ss_file.c
@@ -4,21 +4,46 @@
 #include <libavformat/avformat.h>
 
 #include <ss_file.h>
+#include <ss_process.h>
 
 void init_fileMap(fileMap * fm){
-    //fm->filename = (char *) calloc(1,sizeof(char));
-    //fm->ops = (char  *) calloc(1,sizeof(char));
     fm->fmt_ctx = (AVFormatContext *) calloc(1,sizeof(AVFormatContext));
     fm->ofmt = (AVOutputFormat *) calloc(1,sizeof(AVOutputFormat));
 }
 void free_fileMap(fileMap * fm){
-    //free(fm->filename);
-    //free(fm->ops);
     av_log(NULL,AV_LOG_DEBUG,"before free_fileMap.\n");
     free(fm->fmt_ctx);
     free(fm->ofmt);
     av_log(NULL,AV_LOG_DEBUG,"free_fileMap.\n");
 }
+
+int set_inputs(meetPro * meeting){
+    int ret;
+    meeting->video_main->input_fm->fmt_ctx=NULL;
+    meeting->audio_individual->input_fm->fmt_ctx=NULL;
+    if ((ret = avformat_open_input(&meeting->video_main->input_fm->fmt_ctx, meeting->video_main->input_fm->filename, 0, &meeting->video_main->input_fm->ops)) < 0) {
+        av_log(NULL,AV_LOG_ERROR,"Could not open input video file.\n");
+        return ret;
+    }
+    if ((ret = avformat_find_stream_info(meeting->video_main->input_fm->fmt_ctx, 0)) < 0) {
+        av_log(NULL,AV_LOG_ERROR, "Failed to retrieve input video stream information\n");
+        return ret;
+    }
+    if ((ret = avformat_open_input(&meeting->audio_individual->input_fm->fmt_ctx, meeting->audio_individual->input_fm->filename, 0, &meeting->audio_individual->input_fm->ops)) < 0) {
+        av_log(NULL,AV_LOG_ERROR, "Could not open input audio file.\n");
+        return ret;
+    }
+    if ((ret = avformat_find_stream_info(meeting->audio_individual->input_fm->fmt_ctx, 0)) < 0) {
+        av_log(NULL,AV_LOG_ERROR, "Failed to retrieve input audio stream information\n");
+        return ret;
+    }
+    av_log(NULL,AV_LOG_INFO,"===========Input Information==========\n");
+    av_dump_format(meeting->video_main->input_fm->fmt_ctx, 0, meeting->video_main->input_fm->filename, 0);
+    av_dump_format(meeting->audio_individual->input_fm->fmt_ctx, 0, meeting->audio_individual->input_fm->filename, 0);
+    av_log(NULL,AV_LOG_INFO,"======================================\n");
+    return 0;
+}
+
 
 
 
--- test/structest.c
+++ test/structest.c
@@ -12,16 +12,28 @@ int main(int argc,char **argv){
         av_log(NULL,AV_LOG_ERROR,"usage: %s <input video file> <input audio file> <output file>\n",argv[0]);
         //goto end;
     }
-    meeting1 = (meetPro *) calloc(1,sizeof(meetPro));
     av_log_set_level(AV_LOG_DEBUG);
-    init_meetPro(meeting1);
-    meeting1->video_main->input_fm->filename=argv[1];
-    av_log(NULL,AV_LOG_DEBUG,"%s\n",meeting1->video_main->input_fm->filename);
     av_register_all();
     avformat_network_init();
-    av_log(NULL,AV_LOG_DEBUG,"before free\n");
+    meeting1 = (meetPro *) calloc(1,sizeof(meetPro));
+    init_meetPro(meeting1);
+
+    meeting1->video_main->input_fm->filename=argv[1];
+    meeting1->audio_individual->input_fm->filename=argv[2];
+    meeting1->output_main->filename=argv[3];
+
+    av_log(NULL,AV_LOG_DEBUG,"%s\n",meeting1->video_main->input_fm->filename);
+    av_log(NULL,AV_LOG_DEBUG,"%s\n",meeting1->audio_individual->input_fm->filename);
+    av_log(NULL,AV_LOG_DEBUG,"%s\n",meeting1->output_main->filename);
+    
+    av_dict_set(&meeting1->video_main->input_fm->ops,"protocol_whitelist","file,udp,rtp",0);
+    av_dict_set(&meeting1->audio_individual->input_fm->ops,"protocol_whitelist","file,udp,rtp",0);
+    if((ret = set_inputs(meeting1))<0){
+        av_log(NULL,AV_LOG_ERROR,"error occred while set inputs.\n");
+        //goto end;
+    }else   av_log(NULL,AV_LOG_DEBUG,"successed set inputs\n");
+   
     free_meetPro(meeting1);
-    av_log(NULL,AV_LOG_DEBUG,"after free\n");
     free(meeting1);
 
     return 0;

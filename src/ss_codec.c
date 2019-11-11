69c6683053257878d26ca1b4765f745bbfdec58b
zhengxiaoyu <zhengxiaoyu@eb6960.(none)>
2019-11-11 17:49:51 +0800

    添加了ss_codec.c文件及init/free codecPro内存的函数

---
+++ src/ss_codec.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <libavcodec/avcodec.h>
+
+#include <ss_codec.h>
+
+void init_codecMap(codecMap * cm){
+    cm->codec = (AVCodec *) calloc(1,sizeof(AVCodec));
+    cm->dec = (AVCodec *) calloc(1,sizeof(AVCodec));
+    cm->codec_ctx = (AVCodecContext *) calloc(1,sizeof(AVCodecContext));
+    cm->dec_ctx = (AVCodecContext *) calloc(1,sizeof(AVCodecContext));
+}
+void free_codecMap(codecMap * cm){
+    av_log(NULL,AV_LOG_DEBUG,"before free_codecMap.\n");
+    free(cm->codec);
+    free(cm->dec);
+    free(cm->codec_ctx);
+    free(cm->dec_ctx);
+    av_log(NULL,AV_LOG_DEBUG,"free_codecMap.\n");
+}
+

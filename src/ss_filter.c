79048c15ffb55d9f8b529e9eb216700cd83a706c
zhengxiaoyu <zhengxiaoyu@eb6960.(none)>
2019-11-13 15:20:57 +0800

    添加了使用filter调整帧的函数filting()

--- src/ss_filter.c
+++ src/ss_filter.c
@@ -21,7 +21,6 @@ void free_filterMap(filterMap * fM){
     free(fM->filter_graph);
     av_log(NULL,AV_LOG_DEBUG,"free_filterMap.\n");
 }
-
 int init_filters(streamMap * stream){
     char args[512];
     int ret = 0;
@@ -176,5 +175,29 @@ int init_filters(streamMap * stream){
         av_buffersink_set_frame_size(stream->filtermap->buffersink_ctx,1024);
     return 0;
 }
-
+int filting(AVFrame *frame,AVFrame *filt_frame,AVFilterContext *buffersrc_ctx,AVFilterContext *buffersink_ctx){
+    int ret;
+    if(frame){
+        ret = av_buffersrc_add_frame(buffersrc_ctx,frame);
+        if(ret<0){
+            av_log(NULL,AV_LOG_ERROR,"error while feeding 1 frame to filter\n");
+            return -1;
+        }
+        else av_log(NULL,AV_LOG_DEBUG,"send 1 frame to filter ok\n");
+    }
+    ret = av_buffersink_get_frame(buffersink_ctx,filt_frame);
+    if(ret == AVERROR(EAGAIN)){
+        av_log(NULL,AV_LOG_DEBUG,"the filter need more frame\n");
+        return ret;
+    }else if(ret == AVERROR_EOF){
+        av_log(NULL,AV_LOG_DEBUG,"the filter is eof\n");
+        return ret;
+    }
+    else if(ret <0){
+        av_log(NULL,AV_LOG_ERROR,"error while receive frame from filter\n");
+        return -1;
+    } 
+    av_log(NULL,AV_LOG_DEBUG,"got 1 frame from filter\n");
+    return 0;
+}
 

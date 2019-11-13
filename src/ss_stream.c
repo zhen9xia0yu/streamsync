577656da509a57ea18af8ef78d10e40a3c4d5ce7
zhengxiaoyu <zhengxiaoyu@eb6960.(none)>
2019-11-13 15:23:29 +0800

    添加了设置packet_pts和write packet函数

--- src/ss_stream.c
+++ src/ss_stream.c
@@ -27,6 +27,28 @@ void init_packet(AVPacket *packet){
     packet->data=NULL;
     packet->size=0;
 }
+int set_pts(AVPacket *pkt,AVStream *stream, int pkt_index){
+    AVRational time_base1=stream->time_base;
+    int64_t calc_duration=(double)AV_TIME_BASE/av_q2d(stream->r_frame_rate);
+    pkt->pts=(double)(pkt_index*calc_duration)/(double)(av_q2d(time_base1)*AV_TIME_BASE);
+    pkt->dts=pkt->pts;
+    pkt->duration=(double)calc_duration/(double)(av_q2d(time_base1)*AV_TIME_BASE);
+    //fprintf(stdout,"set new pts&dts.\n");
+    return 0;
+}
+int write_pkt(AVPacket *pkt,AVStream *in_stream,AVStream *out_stream,int stream_index,fileMap *fm){
+    int ret;
+    if(stream_index)    av_packet_rescale_ts(pkt,out_stream->codec->time_base,out_stream->time_base);
+    else   av_packet_rescale_ts(pkt,in_stream->time_base,out_stream->time_base);
+    pkt->pos = -1;
+    pkt->stream_index=stream_index;
+    av_log(NULL,AV_LOG_INFO,"write 1 pkt.pts=%d pkt.dts=%d pkt.duration=%d pkt.size=%d\n",pkt->pts,pkt->dts,pkt->duration,pkt->size);
+    if (av_interleaved_write_frame(fm->fmt_ctx,pkt) < 0) {
+        av_log(NULL,AV_LOG_ERROR, "Error write packet\n");
+        return -1;
+    } 
+    return 0;
+}
 
 
 

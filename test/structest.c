9a38258a34ca3e3b8e52668fb297abac306a1a7f
zhengxiaoyu <zhengxiaoyu@eb6960.(none)>
2019-11-13 11:26:48 +0800

    添加了设置filter部分测试代码

--- test/structest.c
+++ test/structest.c
@@ -6,7 +6,7 @@
 
 int main(int argc,char **argv){
     int ret,i;
-    static meetPro *meeting1;
+    static meetPro *meeting;
 
     if(argc!=4){
         av_log(NULL,AV_LOG_ERROR,"usage: %s <input video file> <input audio file> <output file>\n",argv[0]);
@@ -16,32 +16,55 @@ int main(int argc,char **argv){
     av_log_set_level(AV_LOG_DEBUG);
     av_register_all();
     avformat_network_init();
-    meeting1 = (meetPro *) calloc(1,sizeof(meetPro));
-    init_meetPro(meeting1);
+    meeting = (meetPro *) calloc(1,sizeof(meetPro));
+    init_meetPro(meeting);
 
-    meeting1->video_main->input_fm->filename=argv[1];
-    meeting1->audio_individual->input_fm->filename=argv[2];
-    meeting1->output_main->filename=argv[3];
+    meeting->video_main->input_fm->filename=argv[1];
+    meeting->audio_individual->input_fm->filename=argv[2];
+    meeting->output_main->filename=argv[3];
+    meeting->audio_individual->filtermap->descr="aresample=44100";
 
-    av_log(NULL,AV_LOG_DEBUG,"%s\n",meeting1->video_main->input_fm->filename);
-    av_log(NULL,AV_LOG_DEBUG,"%s\n",meeting1->audio_individual->input_fm->filename);
-    av_log(NULL,AV_LOG_DEBUG,"%s\n",meeting1->output_main->filename);
+    av_log(NULL,AV_LOG_DEBUG,"%s\n",meeting->video_main->input_fm->filename);
+    av_log(NULL,AV_LOG_DEBUG,"%s\n",meeting->audio_individual->input_fm->filename);
+    av_log(NULL,AV_LOG_DEBUG,"%s\n",meeting->output_main->filename);
     
     //set input
-    av_dict_set(&meeting1->video_main->input_fm->ops,"protocol_whitelist","file,udp,rtp",0);
-    av_dict_set(&meeting1->audio_individual->input_fm->ops,"protocol_whitelist","file,udp,rtp",0);
-    if((ret = set_inputs(meeting1))<0){
+    av_dict_set(&meeting->video_main->input_fm->ops,"protocol_whitelist","file,udp,rtp",0);
+    av_dict_set(&meeting->audio_individual->input_fm->ops,"protocol_whitelist","file,udp,rtp",0);
+    if((ret = set_inputs(meeting))<0){
         av_log(NULL,AV_LOG_ERROR,"error occred while set inputs.\n");
         //goto end;
     }else   av_log(NULL,AV_LOG_DEBUG,"successed set inputs\n");
     //set output & encoders
-   if((ret = set_outputs(meeting1))<0){
+   if((ret = set_outputs(meeting))<0){
         av_log(NULL,AV_LOG_ERROR,"error occred while set outputs.\n");
         //goto end;
     }else   av_log(NULL,AV_LOG_DEBUG,"successed set outputs.\n");
-   
-    free_meetPro(meeting1);
-    free(meeting1);
+   //set decoders
+    //if((ret = set_decoder(ifmt_ctx_a,0,&audio_dec,dec_a_ctx))<0){
+    //if((ret = set_decoder(meeting1->audio_individual->input_fm->fmt_ctx,0,meeting1->audio_individual->codecmap->dec,meeting1->audio_individual->codecmap->dec))<0){
+    //if((ret = set_decoder(meeting1,0))<0){
+    if((ret = set_decoder(meeting->audio_individual,0))<0){
+        av_log(NULL,AV_LOG_ERROR,"error occurred when open audio decodec.\n");
+        //goto end;
+    }else   av_log(NULL,AV_LOG_DEBUG,"sucessed set decoder: audio\n");
+
+    av_log(NULL,AV_LOG_DEBUG,"dec_a->samplerate=%d\n",meeting->audio_individual->codecmap->dec_ctx->sample_rate);
+    av_log(NULL,AV_LOG_DEBUG,"codec_a->samplerate=%d\n",meeting->audio_individual->codecmap->codec_ctx->sample_rate);
+    av_log(NULL,AV_LOG_DEBUG,"ifmt->samplerate=%d\n",meeting->audio_individual->input_fm->fmt_ctx->streams[0]->codec->sample_rate);
+
+    //init filters
+    
+    if(init_filters(meeting->audio_individual)<0){
+        av_log(NULL,AV_LOG_ERROR,"could not init audio filter.\n");
+        //goto end;
+    }else   av_log(NULL,AV_LOG_DEBUG,"successed init filter: audio\n");
+    
+    
+
+
+    free_meetPro(meeting);
+    free(meeting);
 
     return 0;
 }

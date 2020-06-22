126177d9d1e20bb79fb1523e5b6182490854b3a3
zhengxiaoyu <zhengxiaoyu@ebupt.com>
2020-06-22 18:23:12 +0800

    调整了最后读完三路流的代码

--- test/pure_muxer_2strm.c
+++ test/pure_muxer_2strm.c
@@ -280,33 +280,10 @@ int main(int argc,char **argv){
         }else {
             av_log(NULL,AV_LOG_DEBUG,"the audio file is over\n");
         }
-        //finish video auxi
-        ifmt_ctx=sm_v_auxi->input_fm->fmt_ctx;
-        in_stream=ifmt_ctx->streams[0];
-        out_stream=meeting->output_auxi->fmt_ctx->streams[0];
-        if(av_read_frame(ifmt_ctx,&vpkt_auxi)>=0){
-            av_log(NULL,AV_LOG_DEBUG,"the video auxi file is not over\n");
-            do{
-                ret = set_pts(&vpkt_auxi,in_stream,sm_v_auxi->cur_index_pkt_in);
-                if(ret<0){
-                    av_log(NULL,AV_LOG_ERROR,"could not set pts\n");
-                    goto end;
-                }
-                sm_v_auxi->cur_index_pkt_in++;
-                sm_v_auxi->cur_pts=vpkt_auxi.pts;
-                av_log(NULL,AV_LOG_INFO,"video: ");
-                ret = write_pkt(&vpkt_auxi,in_stream,out_stream,0,meeting->output_auxi,0);
-                av_packet_unref(&vpkt_auxi);
-                if(ret<0){
-                    av_log(NULL,AV_LOG_ERROR,"error occured while write 1 vpkt\n");
-                    goto end;
-                }
-            }while(av_read_frame(ifmt_ctx,&vpkt_auxi)>=0);
-        }
     }
 
     if(vfile_auxi_over){
-        //finish auido file
+        //finish audio file
         ifmt_ctx = sm_a->input_fm->fmt_ctx;
         in_stream=ifmt_ctx->streams[0];
         out_stream=meeting->output->fmt_ctx->streams[1];
@@ -332,29 +309,6 @@ int main(int argc,char **argv){
             }while(av_read_frame(ifmt_ctx,&apkt)>=0);
         }else {
             av_log(NULL,AV_LOG_DEBUG,"the audio file is over\n");
-        }
-        //finish video main
-        ifmt_ctx=sm_v_main->input_fm->fmt_ctx;
-        in_stream=ifmt_ctx->streams[0];
-        out_stream=meeting->output->fmt_ctx->streams[0];
-        if(av_read_frame(ifmt_ctx,&vpkt)>=0){
-            av_log(NULL,AV_LOG_DEBUG,"the video auxi file is not over\n");
-            do{
-                ret = set_pts(&vpkt,in_stream,sm_v_main->cur_index_pkt_in);
-                if(ret<0){
-                    av_log(NULL,AV_LOG_ERROR,"could not set pts\n");
-                    goto end;
-                }
-                sm_v_main->cur_index_pkt_in++;
-                sm_v_main->cur_pts=vpkt.pts;
-                av_log(NULL,AV_LOG_INFO,"video: ");
-                ret = write_pkt(&vpkt,in_stream,out_stream,0,meeting->output,0);
-                av_packet_unref(&vpkt);
-                if(ret<0){
-                    av_log(NULL,AV_LOG_ERROR,"error occured while write 1 vpkt\n");
-                    goto end;
-                }
-            }while(av_read_frame(ifmt_ctx,&vpkt)>=0);
         }
     }
 

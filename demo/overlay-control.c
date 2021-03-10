841977d7ea15a7f33bb6b36c4467c65fba31a5e9
zhengxiaoyu <zhengxiaoyu@ebupt.com>
2021-03-10 01:36:55 -0500

    adjust gif_duration_s type from int to double and fix the first pts of overlayframe to smooth the gif loop

--- demo/overlay-control.c
+++ demo/overlay-control.c
@@ -56,8 +56,8 @@ int main( int argc, char **argv){
 	//meeting->video->filtermap->descr	= "movie=rocket.gif[wm];[in][wm]overlay=5:5[out]";
 	//meeting->video->filtermap->descr	= "movie=rocket.gif[wm];[in][wm]overlay=(man_w-overlay_w)/2:(main_h-overlay_h)/2[out]";
 	//meeting->video->filtermap->descr	= "movie=logo.png[wm];[in][wm]overlay=((main_w-overlay_w)/2):((main_h-overlay_h)/2)[out]";
-	//meeting->video->filtermap->descr	= "movie=springrocket.gif[wm];[in][wm]overlay=((main_w-overlay_w)/2):((main_h-overlay_h)/2)[out]";
-	meeting->video->filtermap->descr	= "movie=whiterocket-4s.gif[wm];[in][wm]overlay=((main_w-overlay_w)/2):((main_h-overlay_h)/2)[out]";
+	meeting->video->filtermap->descr	= "movie=circlerocket-2.4s.gif[wm];[in][wm]overlay=((main_w-overlay_w)/2):((main_h-overlay_h)/2)[out]";
+	//meeting->video->filtermap->descr	= "movie=whiterocket-4s.gif[wm];[in][wm]overlay=((main_w-overlay_w)/2):((main_h-overlay_h)/2)[out]";
 	//meeting->video->filtermap->descr	= "movie=minibanana.gif[wm];[in][wm]overlay=((main_w-overlay_w)/2):((main_h-overlay_h)/2)[out]";
 	//meeting->video->filtermap->descr	= "boxblur";
 	//meeting->video->filtermap->descr	= "hflip";
@@ -71,7 +71,7 @@ int main( int argc, char **argv){
 	meeting->video->cur_index_pkt_out	= 0;
 	const char * bitrate			= "2500k";
 	int ret 				= 0;
-	int gif_duration_s			= 4;
+	double gif_duration_s			= 2.4;
 
 	/*开启网络流接收通道*/
 	av_dict_set(&meeting->video->input_fm->ops,"protocol_whitelist","file,udp,rtp",0);
@@ -168,14 +168,13 @@ int main( int argc, char **argv){
 			/*adjust the frame pts to sync with gif pts*/
 			/*must synced with the filter's effeciently time*/
 			if ((sm_v->cur_index_pkt_in - gif_first_pkt_index) % gif_increment_pkt_index == 0 ){
+				frames[0]->pts = av_frame_get_best_effort_timestamp(frames[0]);
 				overlayed1stFrame_pts = frames[0]->pts;
 				av_log(NULL,AV_LOG_DEBUG,">>>>>>>>overlayed1st frame pts : %"PRId64"<<<<<<<<\n",overlayed1stFrame_pts);
 				if(init_filters(meeting->video)<0){
 				    av_log(NULL,AV_LOG_ERROR,"could not init video filter.\n");
 				    goto end;
 				}else   av_log(NULL,AV_LOG_DEBUG,"successed init filter: video\n");
-	
-
 			}	
 			/*overlay生效时间段*/
 			//if(sm_v->cur_index_pkt_in >= 100 && sm_v->cur_index_pkt_in <= 175){
